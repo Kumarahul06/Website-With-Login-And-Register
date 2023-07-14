b3cb336af29f0277ac3767fad381100fe8602e19
Kumarahul06 <kumarahul1321@gmail.com>
2023-07-14 21:05:09 +0530

    login page

--- script.js
+++ script.js
@@ -2,6 +2,7 @@ const logregBox = document.querySelector('.logreg-box');
 const loginLink = document.querySelector('.login-link');
 const registerLink = document.querySelector('.register-link');
 
-registerLink.addEventListener('click', () => {
+registerLink.addEventListener('click', () => 
+{
     logregBox.classList.add('active');
-});
+});     
